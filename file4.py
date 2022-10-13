9b444aa889efcbca3aea40de7ad407d841f5a4dc
ssmasriharsha <115638606+ssmasriharsha@users.noreply.github.com>
2022-10-13 23:14:57 +0530

    Update file4.py

--- file4.py
+++ file4.py
@@ -14,6 +14,21 @@ class Solution(object):
                 r = mid2 - 1
             else:
                 return (max(L1, L2) + min(R1, R2)) / 2.0
+            int min = nums[0], max = nums[0], n = nums.length;
+        for (int x : nums) {
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+        }
+        if (min == max) return 0; // All elements are the same
+        int bucketSize = (int) Math.ceil((double) (max - min) / (n - 1));
+        int[] minBucket = new int[n];
+        int[] maxBucket = new int[n];
+        Arrays.fill(minBucket, Integer.MAX_VALUE);
+        Arrays.fill(maxBucket, Integer.MIN_VALUE);
+        for (int x : nums) {
+            int idx = (x - min) / bucketSize;
+            minBucket[idx] = Math.min(x, minBucket[idx]);
+            maxBucket[idx] = Math.max(x, maxBucket[idx]);
 
 
 if __name__ == '__main__':
