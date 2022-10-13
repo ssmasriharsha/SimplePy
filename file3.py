59e489382193fe08e8d44eb90c6c4a0d3ebfaa60
ssmasriharsha <115638606+ssmasriharsha@users.noreply.github.com>
2022-10-13 23:14:09 +0530

    Update file3.py

--- file3.py
+++ file3.py
@@ -10,3 +10,10 @@ class Solution(object):
             charMap[ord(s[j])] = j
             max_len = max(max_len, j - i + 1)
         return max_len
+    
+    if (min == max) return 0; // All elements are the same
+        int bucketSize = (int) Math.ceil((double) (max - min) / (n - 1));
+        int[] minBucket = new int[n];
+        int[] maxBucket = new int[n];
+        Arrays.fill(minBucket, Integer.MAX_VALUE);
+        Arrays.fill(maxBucket, Integer.MIN_VALUE);
